f7644a4b64ee25f684e191232c155f91e95f7dd7
kotmawszy <71193923+kotmawszy@users.noreply.github.com>
2020-09-17 14:19:58 +0200

    Update dzie.cpp

--- 17.09.2020/Dzielniki/dzie.cpp
+++ 17.09.2020/Dzielniki/dzie.cpp
@@ -1,1 +1,20 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	long long n;
+	cin>>n;
+	for(int i = 1; i <= sqrt(n); i++){
+		if(n % i == 0){
+			cout<<i<<endl;			
+		}		
+	}
+	for(int i=sqrt(n); i>=1; i--){
+		if(n % i == 0 and n/i != i){
+			cout<<n/i<<endl;
+		}
+	}
+}
+ 
