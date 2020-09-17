be2bc66c67666037237073ff144cb7292c69616b
kotmawszy <71193923+kotmawszy@users.noreply.github.com>
2020-09-17 14:19:05 +0200

    Update mon.cpp

--- 17.09.2020/Monety/mon.cpp
+++ 17.09.2020/Monety/mon.cpp
@@ -1,1 +1,22 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(0);
+	int n,a,ilosc1=0,ilosc0=0;
+	cin>>n;
+	for(int i;i<n;i++){
+		cin>>a;
+		if(a==1){
+			ilosc1++;
+		}
+		else{
+			ilosc0++;
+		}
+	}
+	if(ilosc1>ilosc0){
+		cout<<ilosc0;
+	}
+	else{
+		cout<<ilosc1;
+	}
+}
