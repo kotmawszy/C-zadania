26743efde7902a39845183c1e112ce53989fdc7c
kotmawszy <71193923+kotmawszy@users.noreply.github.com>
2020-09-17 11:54:41 +0200

    Update Gra_w_lewo_i_w_prawo.cpp

--- Gra_w_lewo_i_w_prawo.cpp
+++ Gra_w_lewo_i_w_prawo.cpp
@@ -4,6 +4,5 @@ using namespace std;
 int main() {
     int n,a,b;
     cin>>n>>a>>b;
-    if(a<b) cout<<(a%2 == b%2 ? 1 : 2);
-    else cout<<(a%2 == b%2 ? 2 : 1);
+    cout<<(a<b xor a%2 == b%2)+1;
 }
