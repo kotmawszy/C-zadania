2ba1d9c7680579717e5c08056972345b93741d20
kotmawszy <71193923+kotmawszy@users.noreply.github.com>
2020-09-17 17:22:11 +0200

    Update dzie.cpp

--- 17.09.2020/Dzielniki/dzie.cpp
+++ 17.09.2020/Dzielniki/dzie.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main() {
 	long long n;
 	cin>>n;
-	for(int i=1; i*i<=n; i++) if(!n%i) cout<<i<<"\n";
-	for(int i=sqrt(n); i>=1; i--) if(!n%i && n/i != i) cout<<n/i<<"\n";
+	for(int i=1; i<=sqrt(n); i++) if(n%i == 0) cout<<i<<"\n";
+	for(int i=sqrt(n); i>=1; i--) if(n%i == 0 && n/i != i) cout<<n/i<<"\n";
 }
