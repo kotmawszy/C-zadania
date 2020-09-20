853889c171336a1257449a047272bd4ccf485dc5
kotmawszy <kotmawszy@gmail.com>
2020-09-20 20:21:02 +0200

    Added dzie.py

---
+++ 17.09.2020/Dzielniki/dzie.py
@@ -0,0 +1,11 @@
+import math
+n = int(input())
+
+for i in range(1, int(math.sqrt(n))):
+    if(n % i == 0):
+        print(i)
+
+
+for i in range(int(math.sqrt(n)), 0, -1):
+    if(n % i == 0 and n/i != i):
+        print(int(n/i))
