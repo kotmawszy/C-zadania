65525d7c4b8e9cc994842ea60a19978b44ddc146
kotmawszy <71193923+kotmawszy@users.noreply.github.com>
2020-09-23 14:33:44 +0200

    Update mon.py

--- 17.09.2020/Monety/mon.py
+++ 17.09.2020/Monety/mon.py
@@ -1,14 +1,10 @@
-n = int( input() )
+n = int(input())
 i1 = 0
 i0 = 0
 for i in range(n):
-	x=input()
+	x = input()
 	if(x == 1):
 		i1 += 1
 	else:
 		i0 += 1
-
-if(i0<i1):
-	print(i0)
-else:
-	print(i1)
+print(i0 if i0<i1 else i1)
