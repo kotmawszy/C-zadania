066d1429759f88e771272b8e260d47638e0821c1
Filip Krawczyk <71193923+kotmawszy@users.noreply.github.com>
2020-09-26 16:14:22 +0200

    Update cpw.py

--- 24.09.2020/Cyfry_pierwsze/cpw.py
+++ 24.09.2020/Cyfry_pierwsze/cpw.py
@@ -4,7 +4,7 @@ for j in range(n):
     a = 0
     b = input()
     
-    for i in range(0,len(b)):
+    for i in range(len(b)):
         if(b[i] == "2" or b[i] == "3" or b[i] == "5" or b[i] == "7"):
             a += 1
     print(a)
