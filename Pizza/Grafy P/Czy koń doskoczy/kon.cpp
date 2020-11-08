c831789fb1f6baf96afae4ff3bb7682f5746c90e
kotmawszy <kotmawszy@gmail.com>
2020-11-08 15:23:46 +0100

    Dodałem "Czy koń doskoczy"

---
+++ Pizza/Grafy P/Czy koń doskoczy/kon.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool tab[1002][1002];
+bool v[1002][1002];
+int xk,yk,xw,yw;
+bool done = false;
+int n;
+
+void f(int x, int y) {
+  if(done) return;
+	if(v[x][y]==1) return;
+	v[x][y]=1;
+	if(x<0 || x>=n || y<0 || y>=n) return;
+	if(x==xw && y==yw) {
+		done=1;
+		return;
+	}
+	if(tab[x][y]==0) return;
+	f(x-1,y+2);
+	f(x+1,y+2);
+	f(x+2,y+1);
+	f(x+2,y-1);
+	f(x+1,y-2);
+	f(x-1,y-2);
+	f(x-2,y-1);
+	f(x-2,y+1);
+}
+
+int main() {
+	string s;
+	cin>>n;
+	for(int i=0;i<n;i++) {
+		cin>>s;
+		for(int j=0;j<n;j++) {
+			tab[i][j] = (s[j]!='x');
+			if(s[j]=='k') xk=i,yk=j;
+			if(s[j]=='w') xw=i,yw=j;
+		}
+	}
+	f(xk,yk);
+	cout<<(done ? "TAK" : "NIE");
+}
