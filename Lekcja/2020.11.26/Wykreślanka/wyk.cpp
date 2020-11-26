1aeb30d60be835df39cf064cb2355e94fc9597ab
kotmawszy <kotmawszy@gmail.com>
2020-11-26 09:57:12 +0100

    Zmieniłem Lekcja/2020.11.26/Wykreślanka

--- Lekcja/2020.11.26/Wykreślanka/wyk.cpp
+++ Lekcja/2020.11.26/Wykreślanka/wyk.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,a;
-int x=1;
-int s;
-
+int n,a,s,x=1;
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  
+
   cin>>n;
   for(int i=0;i<n;i++) {
     cin>>a;
