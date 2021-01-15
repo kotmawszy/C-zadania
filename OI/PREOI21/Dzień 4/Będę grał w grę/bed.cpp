9ea6267af92e340c9304c0a3c2866d8ba1fe2adc
kotmawszy <kotmawszy@gmail.com>
2021-01-15 21:49:10 +0100

    Poprawiłem zadanie Będę grał w grę z PREOIa

--- OI/PREOI21/Dzień 4/Będę grał w grę/bed.cpp
+++ OI/PREOI21/Dzień 4/Będę grał w grę/bed.cpp
@@ -4,25 +4,26 @@ using namespace std;
 int t,n,m;
 string in;
 bool board[303][303];
-vector<int> g[91003];
+vector<pair<int,int> > g[303][303];
 int v;
-int vis[91003];
-int match[91003];
+int vis[303][303];
+pair<int,int> match[303][303];
 int nr;
-int inz;
-int iw;
+int l,r;
+pair<int,int> z;
 
-bool ma(int x) {
-  vis[x] = nr;
-  for(int w : g[x])
-    if(!match[w]) {
-      match[w] = x;
-      inz++;
+bool ma(int i, int j) {
+  vis[i][j] = nr;
+  for(pair<int,int> w : g[i][j])
+    if(match[w.first][w.second] == z) {
+      match[i][j] = {w.first,w.second};
+      match[w.first][w.second] = {i,j};
       return 1;
     }
-  for(int w : g[x])
-    if(vis[match[w]] != nr && ma(match[w])) {
-      match[w] = x;
+  for(pair<int,int> w : g[i][j])
+    if(vis[match[w.first][w.second].first][match[w.first][w.second].second] != nr && ma(match[w.first][w.second].first, match[w.first][w.second].second)) {
+      match[w.first][w.second] = {i,j};
+      match[i][j] = {w.first,w.second};
       return 1;
     }
   return 0;
@@ -33,49 +34,52 @@ int main() {
   cin.tie(NULL);
   cout.tie(NULL);
 
+  z = {0,0};
+
   cin>>t;
   for(int tt=1;tt<=t;tt++) {
-    iw=0;
     cin>>n>>m;
+    l=r=0;
     for(int i=1;i<=n;i++) {
       cin>>in;
       for(int j=1;j<=m;j++) {
         board[i][j]=(in[j-1]=='.');
         if(board[i][j]) {
-          iw++;
-          if(i>1 && board[i-1][j]) {
-            g[i*m+j-1].push_back((i-1)*m+j-1);
-            g[(i-1)*m+j-1].push_back(i*m+j-1);
-          }
-          if(j>1 && board[i][j-1]) {
-            g[i*m+j-1].push_back(i*m+j-2);
-            g[i*m+j-2].push_back(i*m+j-1);
-          }
+          if((i+j)%2) l++;
+          else r++;
         }
-        vis[i*m+j-1]=0;
-        match[i*m+j-1]=0;
+        g[i][j].clear();
+        vis[i][j]=0;
+        match[i][j]={0,0};
       }
     }
-    if(iw==n*m) {
-      if(n%2 && m%2) cout<<"Piotr\n";
-      else cout<<"Krzysztof\n";
-      for(int i=m;i<=(n+1)*m-1;i++) g[i].clear();
+    if(l!=r) {
+      cout<<"Piotr\n";
       continue;
     }
-    if(iw==0) {
-      cout<<"Krzysztof\n";
-      for(int i=m;i<=(n+1)*m-1;i++) g[i].clear();
-      continue;
+    for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) {
+      if(board[i][j]) {
+        if(i<n && board[i+1][j]) g[i][j].push_back({i+1,j});
+        if(i>1 && board[i-1][j]) g[i][j].push_back({i-1,j});
+        if(j<m && board[i][j+1]) g[i][j].push_back({i,j+1});
+        if(j>1 && board[i][j-1]) g[i][j].push_back({i,j-1});
+      }
     }
-    inz=0;
-    nr=1;
-    for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) if(board[i][j] && vis[i*m+j-1]==0) {
-      ma(i*m+j-1);
-      nr++;
-    }
-    if(iw==inz) cout<<"Krzysztof\n";
-    else cout<<"Piotr\n";
 
-    for(int i=m;i<=(n+1)*m-1;i++) g[i].clear();
+    /*for(int i=1;i<=n;i++)
+      for(int j=1;j<=m;j++) {
+        cout<<"("<<i<<","<<j<<"): ";
+        for(pair<int,int> w : g[i][j]) cout<<"("<<w.first<<","<<w.second<<") ";
+        cout<<"\n";
+      }*/
+
+    nr=0;
+    bool p=0;
+    for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) {
+      nr++;
+      if(board[i][j] && match[i][j]==z && !ma(i,j)) p=1;
+    }
+    if(p) cout<<"Piotr\n";
+    else cout<<"Krzysztof\n";
   }
 }
