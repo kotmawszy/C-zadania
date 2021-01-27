61893b116a661bbcb02bf4ab0cb078478d6e3e0a
kotmawszy <kotmawszy@gmail.com>
2021-01-27 16:50:00 +0100

    Zmieniłem bagna1

--- KoloS/DP/Bagna1/bag.cpp
+++ KoloS/DP/Bagna1/bag.cpp
@@ -1,22 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[1007][1007];
 int pfx[1007][1007];
+int a;
 int n,m;
 
 int main() {
   cin>>n>>m;
-  for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) {
-    cin>>arr[i][j];
-  }
-  pfx[1][1]=arr[1][1];
-  for(int i=2;i<=n;i++) pfx[i][1]=pfx[i-1][1]+arr[i][1];
-  for(int i=2;i<=n;i++) pfx[1][i]=pfx[1][i-1]+arr[1][i];
-  for(int i=2;i<=n;i++) {
-    for(int j=2;j<=m;j++) {
-      pfx[i][j]=max(pfx[i-1][j],pfx[i][j-1])+arr[i][j];
+
+  for(int i=1;i<=n;i++) {
+    for(int j=1;j<=m;j++) {
+      cin>>a;
+      pfx[i][j]=max(pfx[i-1][j],pfx[i][j-1])+a;
     }
   }
-  cout<<pfx[n][m];
+  cout<<pfx[n][m]<<"\n";
 }
