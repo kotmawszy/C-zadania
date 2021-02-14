10d71dbd6fab4e878fd18d4cd366f02923584777
kotmawszy <kotmawszy@gmail.com>
2021-02-14 11:06:12 +0100

    Zmieniłem zakazane indeksy

--- OI/PREOI21/Dzień 5/Zakazane indeksy/zak.cpp
+++ OI/PREOI21/Dzień 5/Zakazane indeksy/zak.cpp
@@ -4,21 +4,9 @@ using namespace std;
 int n;
 string s;
 string c;
-int* p;
-int mx=0;
-
-int kmp(string& x, int f) {
-  p[1]=0;
-  int sum=0;
-  for(int i=2;i<=x.length();i++) {
-    int t = p[i-1];
-    while(t && x[t]!=x[i-1]) t=p[t];
-    if(x[t]==x[i-1]) p[i]=t+1;
-    else p[i]=0;
-    if(p[i]==f && c[i-f-2]=='0') sum++;
-  }
-  return sum;
-}
+int* suf;
+int* lcp; // długość najdłuższego wspólnego prefiksu słowa i oraz i+1
+int* index; // na której pozycji znajduje się index i w tablicy suf
 
 int main() {
   ios_base::sync_with_stdio(0);
@@ -27,16 +15,5 @@ int main() {
 
   cin>>n>>s>>c;
 
-  p = new int[2*(n+1)];
-
-  for(int i=1;i<=n;i++) {
-    for(int j=0;j<=n-i;j++) {
-      string sb = s.substr(j,i);
-      string kk = sb+"#"+s;
-
-      int w = kmp(kk,i) * i;
-      mx = max(mx,w);
-    }
-  }
-  cout<<mx;
+  suf = new int[n+1];
 }
