668d7266b94c5a11f3fa8e74bee7fa957493ffe0
kotmawszy <kotmawszy@gmail.com>
2021-02-14 11:21:31 +0100

    Zmieniłem Liczby parzystocyfrowe

--- Lekcja/2021.02.04/Liczby parzystocyfrowe/par1.cpp
+++ Lekcja/2021.02.04/Liczby parzystocyfrowe/par1.cpp
@@ -2,30 +2,20 @@
 using namespace std;
 
 long long n;
-string a;
-int p=10;
-int q=5;
+stack<char> s;
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  cin>>a;
-  long long base=1;
-  long long x=0;
-  for(int i=0;i<a.size();i++) {
-    x+=(a[a.size()-i-1]-'0')*base;
-    base*=p;
-  }
-
-  stack<int> s;
-  while(x) {
-    s.push(x%q);
-    x/=q;
+  cin>>n;
+  while(n) {
+    s.push(2*(n%5)+48);
+    n/=5;
   }
   while(!s.empty()) {
-    cout<<s.top()*2;
+    cout<<s.top();
     s.pop();
   }
 }
