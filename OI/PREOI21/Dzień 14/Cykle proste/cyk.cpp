bd853c2a2ec0b048fea5787ebee62f16561105f5
kotmawszy <kotmawszy@gmail.com>
2021-04-07 10:13:45 +0200

    Zmieniłem Cykle proste

--- OI/PREOI21/Dzień 14/Cykle proste/cyk.cpp
+++ OI/PREOI21/Dzień 14/Cykle proste/cyk.cpp
@@ -2,21 +2,11 @@
 using namespace std;
 
 int n,m;
-vector<int>* g;
+vector<pair<int,int> >* g;
+pair<int,int>* e;
 int a,b;
-int s;
-int p,q;
-int* vis;
-bool found,ok;
-stack<int> st;
-
-void dfs(int v) {
-	if(vis[v]) return;
-	vis[v]=1;
-	int iw=0;
-	for(int u : g[v]) if(u<=q && u>=p) {dfs(u);iw++;}
-	if(iw!=2) ok=0;
-}
+int* deg; // stopień danego wierzchołka (krawędzie tylko do wierzchołków w danym przedziale)
+pair<int,int>* einterval;
 
 int main() {
 	ios_base::sync_with_stdio(0);
@@ -24,25 +14,54 @@ int main() {
 	cout.tie(NULL);
 
 	cin>>n>>m;
-	g = new vector<int>[n+1];
-	vis = new int[n+1];
 
-	while(m--) {
-		cin>>a>>b;
-		g[a].push_back(b);
-		g[b].push_back(a);
+	g = new vector<pair<int,int> >[n+1];
+	e = new pair<int,int>[m+1];
+	deg = new int[n+1];
+	einterval = new int[m+1];
+	for(int i=1;i<=n;i++) {
+		deg[i]=0;
 	}
-	for(int i=1;i<=n;i++) sort(g[i].begin(), g[i].end());
 
+	for(int i=1;i<=m;i++) {
+		cin>>a>>b;
+		g[a].push_back({b,i});
+		g[b].push_back({a,i});
+		e[i] = {a,b};
+	}
 
-	for(p=1;p<=n;p++) {
-		for(q=p+1;q<=n;q++) {
-			for(int i=1;i<=n;i++) vis[i]=0;
-			ok=1;
-			dfs(p);
-			for(int i=p;i<=q;i++) if(!vis[i]) ok=0;
-			if(ok) s++;
+	int zle=0; // ile wierzchołków w przedziale ma
+	int p=1; // początek gąsiennicy
+	for(int q=1;q<=n;q++) { // koniec gasiennicy
+		// dodanie q wierzchołka do przedziału
+		for(pair<int,int> u : g[q]) if(p <= u.first && u.first <= q) {
+			// dla każdego sąsiada należącego do przedziału
+			deg[q]++;
+			deg[u.first]++;
+			if(deg[q]==3) zle++;
+			if(deg[u.first]==3) zle++;
+			einterval[u.second].first = q;
+		}
+		// usunięcie krawędzi z początku gąsiennicy
+		while(zle) {
+			for(pair<int,int> u : g[p]) if(p <= u.first && u.first <= q) {
+				// dla każdego sąsiada należącego do przedziału
+				if(deg[q]==3) zle--;
+				if(deg[u.first]==3) zle--;
+				deg[p]--;
+				deg[u.first]--;
+				einterval[u.second].second = p;
+			}
+			p++;
 		}
 	}
-	cout<<s<<"\n";
+	while(p<=n) {
+		for(pair<int,int> u : g[p]) if(p <= u.first && u.first <= q) {
+			// dla każdego sąsiada należącego do przedziału
+			deg[p]--;
+			deg[u.first]--;
+			einterval[u.second].second = p;
+		}
+		p++;
+	}
 }
