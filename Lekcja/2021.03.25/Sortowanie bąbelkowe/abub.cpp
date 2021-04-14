fdbe93ec4dc1f5a6b6e96416b2b34c87ad16df4a
kotmawszy <kotmawszy@gmail.com>
2021-04-14 22:22:50 +0200

    Zmieniłem abub

--- Lekcja/2021.03.25/Sortowanie bąbelkowe/abub.cpp
+++ Lekcja/2021.03.25/Sortowanie bąbelkowe/abub.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+int tab[100007];
+
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(NULL);
   cout.tie(NULL);
-  int n;
+
   cin>>n;
-  int tab[n];
   for(int i=0;i<n;i++) cin>>tab[i];
 
   for(int j=0;j<n-1;j++) {
@@ -20,7 +22,5 @@ int main() {
     }
   }
 
-  for(int i=0;i<n;i++) {
-    cout<<tab[i]<<" ";
-  }
+  for(int i=0;i<n;i++) cout<<tab[i]<<" ";
 }
